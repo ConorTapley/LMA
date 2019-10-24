c22598896eeafb8c96f911cd5fa54e6b93755b70
ConorTapley <34166407+ConorTapley@users.noreply.github.com>
2019-10-24 07:46:53 -0800

    adding Controller Vibration

---
+++ LMA_Training/Assets/Scripts/IncertLMA.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IncertLMA : MonoBehaviour
+{
+    private OVRGrabbable ovrGrabbable;
+    public OVRInput.Button grabButton;
+
+    void Start()
+    {
+        ovrGrabbable = GetComponent<OVRGrabbable>();
+    }
+
+    
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Mouth"))
+        {
+            Debug.Log("IN MOUTH");
+            VibrationManager.singleton.TriggerVibration(2, OVRInput.Controller.All);
+        }
+    }
+}
---
+++ LMA_Training/Assets/Scripts/VibrationManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VibrationManager : MonoBehaviour
+{
+
+    public static VibrationManager singleton;
+    
+    void Start()
+    {
+        if (singleton && singleton != this)
+            Destroy(this);
+        else
+            singleton = this;
+    }
+
+    public void TriggerVibration(int vibrationDuration, OVRInput.Controller controller)
+    {
+        OVRHapticsClip clip = new OVRHapticsClip(vibrationDuration);
+
+        //Trigger vibration on left controller
+        if(controller == OVRInput.Controller.LTouch)
+        {
+            OVRHaptics.LeftChannel.Preempt(clip);
+        }
+
+        //Trigger Vibration on right contrller
+        else if (controller == OVRInput.Controller.RTouch)
+        {
+            OVRHaptics.RightChannel.Preempt(clip);
+        }
+    }
+}
