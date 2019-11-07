3db83b20f558435c4ba73c7c048393424ece0753
ConorTapley <34166407+ConorTapley@users.noreply.github.com>
2019-11-07 14:12:20 -0800

    SceneChanger
    The play button brings you from the playroom scene to the game scene and vis versa

--- LMA_Training/Assets/Scripts/SceneChanger.cs
+++ LMA_Training/Assets/Scripts/SceneChanger.cs
@@ -5,9 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class SceneChanger : MonoBehaviour
 {
+    [SerializeField] private string SceneName;
+
     public void ChangeScene()
     {
-        SceneManager.LoadScene("SceneName");
+        SceneManager.LoadScene(SceneName);
     }
     
 }
