8411adb497cbe82441fce3c8e853735f23e02837
R-Hennessy <56439705+R-Hennessy@users.noreply.github.com>
2019-11-11 01:07:47 +0000

    Change1

---
+++ LMA_Training/Assets/Scripts/TextBoxScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextBoxScript : MonoBehaviour
+{
+    public Text popUpText;
+
+    private GameObject tSpawn;
+
+    void Start()
+    {
+        tSpawn = GameObject.FindGameObjectWithTag("TextBoxSpawn");
+    }
+
+
+
+    void Update()
+    {
+
+    }
+
+    public void TextBox()
+    {
+        Instantiate(popUpText, new Vector3(transform.position.x,transform.position.y,transform.position.z),Quaternion.identity );
+        popUpText.transform.parent = tSpawn.transform; 
+        
+    }
+}
