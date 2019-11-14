bfc2a6b5322ac721a0b73d72d35d1fbfebaf1c58
ConorTapley <34166407+ConorTapley@users.noreply.github.com>
2019-11-14 15:45:16 -0800

    aniamtion button

--- LMA_Training/Assets/Scripts/GameController.cs
+++ LMA_Training/Assets/Scripts/GameController.cs
@@ -1,12 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
     //each part of the game can only be played if the bool for it is true
     [SerializeField] private bool step1 = true, step2 = false, step3 = false, step4 = false, step5 = false, step6 = false, step7 = false, step8 = false;
+
     [SerializeField] private Animator patientAnimator;
+
+    [SerializeField] private Animator removeCaseAnimator;
+    [SerializeField] private Button removeCaseButton;
 
     void Start()
     {
@@ -20,5 +25,11 @@ public class GameController : MonoBehaviour
             patientAnimator.Play("Patient Head Tilt");
             //Debug.Log("Trigger");
         }
+    }
+
+    public void RemoveCase()
+    {
+        removeCaseAnimator.Play("RomoveCase");
+        removeCaseButton.IsActive = false;
     }
 }
