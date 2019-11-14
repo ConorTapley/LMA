7819cdf9f660873a6440df40ad575bbe7a2783ea
ConorTapley <34166407+ConorTapley@users.noreply.github.com>
2019-11-14 10:39:12 -0800

    patient animation trigger
    when the trigger on the controller is pressed the patient head tilt animation plays

---
+++ LMA_Training/Assets/Scripts/GameController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    //each part of the game can only be played if the bool for it is true
+    [SerializeField] private bool step1 = true, step2 = false, step3 = false, step4 = false, step5 = false, step6 = false, step7 = false, step8 = false;
+    [SerializeField] private Animator patientAnimator;
+
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        if(OVRInput.Get(OVRInput.Button.PrimaryIndexTrigger) || OVRInput.Get(OVRInput.Button.SecondaryIndexTrigger))
+        {
+            patientAnimator.Play("Patient Head Tilt");
+            //Debug.Log("Trigger");
+        }
+    }
+}
