500297d3935aca623b538d128424db139c8a8bbf
Keith Connors <c17507803@mydit.ie>
2019-11-15 23:57:36 +0000

    AudioManager
    Prep for Rhys' code to implement audio

---
+++ LMA_Training/Assets/AudioManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public AudioClip m_DialogueStep1;
+    public AudioClip m_DialogueStep2;
+    public AudioClip m_DialogueStep3;
+    public AudioClip m_DialogueStep4;
+    public AudioClip m_DialogueStep5;
+    public AudioClip m_DialogueStep6;
+    public AudioClip m_DialogueStep7;
+    public AudioClip m_DialogueStep8;
+    public AudioClip m_DialogueStep9;
+    public AudioClip m_DialogueStep10;
+    public AudioClip m_DialogueStep11;
+    public AudioClip m_DialogueStep12; 
+
+}
