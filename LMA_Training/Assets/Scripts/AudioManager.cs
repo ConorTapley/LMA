bc4da3ab6254d0512f6db7d0bccaa1fa8443dcda
Keith Connors <c17507803@mydit.ie>
2019-11-16 00:25:13 +0000

    Audio Manager
    ++ Audio Manager
    -Audio Manager will store all of the Dialogue Clips
    -NurseManager (Rhys) will be hooked into the Audio Manager to allow for Dialogue to play through the steps.
    
    ++ Scene Manager script now allows for the user to Quit from the game.
    -Doors to leave the game will be implemented soon.
    -Quit needs to be rigged up for it to work. Possibly does not work within Editor.

--- LMA_Training/Assets/Scripts/AudioManager.cs
+++ LMA_Training/Assets/Scripts/AudioManager.cs
@@ -4,17 +4,24 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
+    //public NurseManager nurseManager;
+
+/**
+ * Dialogue Audio Clips - By steps that require audio
+ */
     public AudioClip m_DialogueStep1;
     public AudioClip m_DialogueStep2;
-    public AudioClip m_DialogueStep3;
     public AudioClip m_DialogueStep4;
-    public AudioClip m_DialogueStep5;
+    public AudioClip m_DialogueStep5_Size1;
+    public AudioClip m_DialogueStep5_Size2;
+    public AudioClip m_DialogueStep5_Size3;
+    public AudioClip m_DialogueStep5_size4;
+    public AudioClip m_DialogueStep5_Size5;
     public AudioClip m_DialogueStep6;
     public AudioClip m_DialogueStep7;
     public AudioClip m_DialogueStep8;
     public AudioClip m_DialogueStep9;
     public AudioClip m_DialogueStep10;
-    public AudioClip m_DialogueStep11;
-    public AudioClip m_DialogueStep12; 
+    public AudioClip m_DialogueStep12;
 
 }
