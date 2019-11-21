38639ae083e5fe222e1bdf47fb755584820927c4
ConorTapley <34166407+ConorTapley@users.noreply.github.com>
2019-11-21 06:10:50 -0800

    made player spawn better

---
+++ LMA_Training/Assets/Scripts/Spawn.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour
+{
+    public GameObject spawn;
+    private Vector3 spawnPosition;
+    private Vector3 spawnRotaion;
+    private bool inSpawn;
+
+    void Start()
+    {
+        spawnPosition = spawn.transform.position;
+        spawnRotaion = spawn.transform.eulerAngles;
+
+        
+        //SpawnPoint();
+    }
+
+    void Update()
+    {
+        if(!inSpawn)
+        {
+            SpawnPoint();
+        }
+    }
+
+    private void SpawnPoint()
+    {
+        this.transform.position = spawnPosition;
+        this.transform.eulerAngles = spawnRotaion;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Spawn"))
+        {
+            Debug.Log("Spawn");
+            inSpawn = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        //SpawnPoint();
+        inSpawn = false;
+    }
+}
---
+++ LMA_Training/Assets/Scripts/SpawnPoint.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPoint : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 startPosition;
+    private Vector3 startRotaion;
+    
+
+    void Start()
+    {
+        player = this.gameObject;
+        startPosition = player.transform.position;
+        startRotaion = player.transform.eulerAngles;
+
+        player.transform.position = startPosition;
+        player.transform.eulerAngles = startRotaion;
+    }
+
+    void Update()
+    {
+        
+    }
+}
