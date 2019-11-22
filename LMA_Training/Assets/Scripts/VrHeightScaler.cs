ebc9892f4a610268c15b4e77fd06a3ae7d4e7bbe
ConorTapley <34166407+ConorTapley@users.noreply.github.com>
2019-11-21 17:22:18 -0800

    Height Scaler

---
+++ LMA_Training/Assets/Scripts/VrHeightScaler.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VrHeightScaler : MonoBehaviour
+{
+    [SerializeField] private float defaultHeight = 1f;
+
+    private void Start()
+    {
+        this.transform.localScale = new Vector3(this.transform.localScale.x, defaultHeight, this.transform.localScale.z);
+    }
+}
