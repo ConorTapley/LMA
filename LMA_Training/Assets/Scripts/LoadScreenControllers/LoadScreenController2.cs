a059e61f97b7ea9e25c6309dab34f695d5ddd113
Eibho <eibhyor@gmail.com>
2019-12-07 22:54:40 +0000

    Working on title scene

---
+++ LMA_Training/Assets/Scripts/LoadScreenControllers/LoadScreenController2.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoadScreenController : MonoBehaviour
+{
+
+    public GameObject LoadBar;
+    public Slider m_slider;
+
+    public float LoadBarFakeTimer = 0.5f;
+    public float fillQuantity = 0.1f;
+
+    public void Start()
+    {
+        StartCoroutine(FillSlider());
+    }
+    public IEnumerator FillSlider()
+    {
+        yield return new WaitForSeconds(LoadBarFakeTimer);
+        m_slider.value += fillQuantity;
+
+        if(m_slider.value >= 0.9f)
+        {
+            SceneManager.LoadScene(1);
+        }
+        StartCoroutine(FillSlider());
+    }
+}
+
---
+++ LMA_Training/Assets/Scripts/TitleSceneManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleSceneManager : MonoBehaviour
+{
+    public int loadSceneIndex;
+    public int WaitTime;
+
+    public void Start()
+    {
+        StartCoroutine(loadNextScene());
+    }
+
+
+    public void Update()
+    {
+        
+    }
+
+   IEnumerator loadNextScene()
+    {
+        yield return new WaitForSeconds(WaitTime);
+        SceneManager.LoadScene(loadSceneIndex);
+
+    }
+
+    
+}
