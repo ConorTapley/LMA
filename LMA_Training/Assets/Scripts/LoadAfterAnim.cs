1a78aeb63613ae1d191064ebc1087ad592db55dd
Eibho <eibhyor@gmail.com>
2019-12-07 18:18:21 +0000

    Finished loading screen

---
+++ LMA_Training/Assets/Scripts/LoadAfterAnim.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadAfterAnim : MonoBehaviour
+{
+    public GameObject loadingBar;
+    public int waitTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        loadingBar.SetActive(false);
+        StartCoroutine(Wait());
+    }
+
+    IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(waitTime);
+        Debug.Log("Wait Time Over");
+
+        loadingBar.SetActive(true);
+        
+    }
+
+
+}
